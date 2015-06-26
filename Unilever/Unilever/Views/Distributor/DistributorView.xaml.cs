b4f4d9defc4f0e2866c1d5c3d5fc30ed7b8f1bc3
keikiyoshi <huynhchanhkiet94@gmail.com>
2015-06-26 22:43:42 +0700

    Update distributor

--- Unilever/Unilever/Views/Distributor/DistributorView.xaml.cs
+++ Unilever/Unilever/Views/Distributor/DistributorView.xaml.cs
@@ -29,7 +29,19 @@ namespace Unilever.Views.Distributor
         private void grdDistributor_MouseDoubleClick(object sender, MouseButtonEventArgs e)
         {
             var item = grdDistributor.SelectedItem as Unilever.DTO.Entity.Distributor;
-            MessageBox.Show(item.Id.ToString());
+            btnDisAdd.Visibility = Visibility.Hidden;
+            btnDisUpdate.Visibility = Visibility.Visible;
+            ChangeInput(item);
+        }
+
+        private void ChangeInput(DTO.Entity.Distributor item)
+        {
+            txtDisId.Text = item.Id.ToString();
+            txtDisName.Text = item.Name;
+            txtDisTimeLimit.Text = item.TimeLimit.ToString();
+            txtDisPhone.Text = item.Phone;
+            txtDisEmail.Text = item.Email;
+            txtDisAddress.Text = item.Address;
         }
 
         private void grdDistributor_MouseRightButtonDown(object sender, MouseButtonEventArgs e)
@@ -41,6 +53,41 @@ namespace Unilever.Views.Distributor
         {
             var item = grdDistributor.SelectedItem as Unilever.DTO.Entity.Distributor;
             new DistributorDAO().Remove(item.Id);
+            grdDistributor.ItemsSource = new DistributorDAO().GetAll();
+        }
+
+        private void btnDisAdd_Click(object sender, RoutedEventArgs e)
+        {
+            DTO.Entity.Distributor item = new DTO.Entity.Distributor();
+            item.Name = txtDisName.Text;
+            item.TimeLimit = int.Parse(txtDisTimeLimit.Text);
+            item.Phone = txtDisPhone.Text;
+            item.Email = txtDisEmail.Text;
+            item.Address = txtDisAddress.Text;
+
+            new DistributorDAO().Add(item);
+            grdDistributor.ItemsSource = new DistributorDAO().GetAll();
+        }
+
+        private void btnDisRefresh_Click(object sender, RoutedEventArgs e)
+        {
+            DTO.Entity.Distributor item = new DTO.Entity.Distributor();
+            ChangeInput(item);
+            btnDisAdd.Visibility = Visibility.Visible;
+            btnDisUpdate.Visibility = Visibility.Hidden;
+        }
+
+        private void btnDisUpdate_Click(object sender, RoutedEventArgs e)
+        {
+            DTO.Entity.Distributor item = new DTO.Entity.Distributor();
+            item.Id = int.Parse(txtDisId.Text);
+            item.Name = txtDisName.Text;
+            item.TimeLimit = int.Parse(txtDisTimeLimit.Text);
+            item.Phone = txtDisPhone.Text;
+            item.Email = txtDisEmail.Text;
+            item.Address = txtDisAddress.Text;
+
+            new DistributorDAO().UpdateInfo(item);
             grdDistributor.ItemsSource = new DistributorDAO().GetAll();
         }
     }
