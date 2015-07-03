105b336013ab07386c496821bf24da0a699f8c0d
quang sơn <lephuocquangson@gmail.com>
2015-07-03 13:03:13 +0700

    Create seller

---
+++ Seller/Seller/Views/InterestOfYears/InterestView.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Seller.Views.InterestOfYears
+{
+    /// <summary>
+    /// Interaction logic for InterestView.xaml
+    /// </summary>
+    public partial class InterestView : Window
+    {
+        public InterestView()
+        {
+            InitializeComponent();
+        }
+    }
+}
