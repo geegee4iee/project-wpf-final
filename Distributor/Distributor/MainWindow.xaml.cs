134c304e59d2fa04d8c5b6bff4b6fa98ddec7b6b
quang sơn <lephuocquangson@gmail.com>
2015-07-04 11:56:44 +0700

    Create View for Distributor

--- Distributor/Distributor/MainWindow.xaml.cs
+++ Distributor/Distributor/MainWindow.xaml.cs
@@ -23,6 +23,103 @@ namespace Distributor
         public MainWindow()
         {
             InitializeComponent();
+            CloseAllTab();
+        }
+
+        private void CloseAllTab()
+        {
+            pnSeller.Visibility = Visibility.Collapsed;
+            pnInfo.Visibility = Visibility.Collapsed;
+
+            pnProduct.Visibility = Visibility.Collapsed;
+            pnCategory.Visibility = Visibility.Collapsed;
+            pnStock.Visibility = Visibility.Collapsed;
+            pnIssue.Visibility = Visibility.Collapsed;
+
+            pnImportExcel.Visibility = Visibility.Collapsed;
+            pnExportExcel.Visibility = Visibility.Collapsed;
+
+            pnSalesReport.Visibility = Visibility.Collapsed;
+            pnStockReport.Visibility = Visibility.Collapsed;
+            pnDebtReport.Visibility = Visibility.Collapsed;
+        }
+
+        private void btnExit_ItemClick(object sender, DevExpress.Xpf.Bars.ItemClickEventArgs e)
+        {
+
+        }
+
+        private void btnInfo_ItemClick(object sender, DevExpress.Xpf.Bars.ItemClickEventArgs e)
+        {
+            CloseAllTab();
+            pnInfo.Visibility = Visibility.Visible;
+        }
+
+        private void btnSeller_ItemClick(object sender, DevExpress.Xpf.Bars.ItemClickEventArgs e)
+        {
+            CloseAllTab();
+            pnSeller.Visibility = Visibility.Visible;
+        }
+
+        private void btnStock_ItemClick(object sender, DevExpress.Xpf.Bars.ItemClickEventArgs e)
+        {
+            CloseAllTab();
+            pnStock.Visibility = Visibility.Visible;
+        }
+
+        private void btnProduct_ItemClick(object sender, DevExpress.Xpf.Bars.ItemClickEventArgs e)
+        {
+            CloseAllTab();
+            pnProduct.Visibility = Visibility.Visible;
+        }
+
+        private void btnCategory_ItemClick(object sender, DevExpress.Xpf.Bars.ItemClickEventArgs e)
+        {
+            CloseAllTab();
+            pnCategory.Visibility = Visibility.Visible;
+        }
+
+        private void btnIssue_ItemClick(object sender, DevExpress.Xpf.Bars.ItemClickEventArgs e)
+        {
+            CloseAllTab();
+            pnIssue.Visibility = Visibility.Visible;
+        }
+
+        private void btnIssueDetail_ItemClick(object sender, DevExpress.Xpf.Bars.ItemClickEventArgs e)
+        {
+            CloseAllTab();
+            pnIssue.Visibility = Visibility.Visible;
+        }
+
+        private void btnSalesReport_ItemClick(object sender, DevExpress.Xpf.Bars.ItemClickEventArgs e)
+        {
+            CloseAllTab();
+            pnSalesReport.Visibility = Visibility.Visible;
+        }
+
+        private void btnDebtReport_ItemClick(object sender, DevExpress.Xpf.Bars.ItemClickEventArgs e)
+        {
+            CloseAllTab();
+            pnDebtReport.Visibility = Visibility.Visible;
+        }
+
+        private void btnStockReport_ItemClick(object sender, DevExpress.Xpf.Bars.ItemClickEventArgs e)
+        {
+            CloseAllTab();
+            pnStockReport.Visibility = Visibility.Visible;
+        }
+
+        private void btnImportExcel_ItemClick(object sender, DevExpress.Xpf.Bars.ItemClickEventArgs e)
+        {
+            CloseAllTab();
+            pnImportExcel.Visibility = Visibility.Visible;
+
+        }
+
+        private void btnExportExcel_ItemClick(object sender, DevExpress.Xpf.Bars.ItemClickEventArgs e)
+        {
+            CloseAllTab();
+            pnExportExcel.Visibility = Visibility.Visible;
         }
     }
 }
